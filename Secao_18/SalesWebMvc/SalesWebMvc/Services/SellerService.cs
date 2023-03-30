0787bd9ae3c7c41ffd5e65fe12ad98d773749610
Manre Torres Cabral <manretc@gmail.com>
2023-03-29 21:07:57 -0300

    Delete seller

--- Secao_18/SalesWebMvc/SalesWebMvc/Services/SellerService.cs
+++ Secao_18/SalesWebMvc/SalesWebMvc/Services/SellerService.cs
@@ -9,7 +9,7 @@ namespace SalesWebMvc.Services
     {
         private readonly SalesWebMvcContext _context;
 
-        public SellerService (SalesWebMvcContext context)
+        public SellerService(SalesWebMvcContext context)
         {
             _context = context;
         }
@@ -22,6 +22,18 @@ namespace SalesWebMvc.Services
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
